455e6bbbeb142fbff2676d68c40543a60846f4c3
MariahCasciola417 <117303840+MariahCasciola@users.noreply.github.com>
2024-10-14 20:39:59 -0500

    page.js route is rendered

---
+++ src/app/dashboard/SideNav.js
@@ -0,0 +1,18 @@
+export default function SideNav() {
+  return (
+    <ul>
+      <li>
+        <a href="default.asp">Gallery</a>
+      </li>
+      <li>
+        <a href="news.asp"><>Letters</></a>
+      </li>
+      <li>
+        <a href="contact.asp">Library</a>
+      </li>
+      <li>
+        <a href="about.asp">Theater</a>
+      </li>
+    </ul>
+  );
+}
